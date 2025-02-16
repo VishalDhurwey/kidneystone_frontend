2add85566266b59a8c46c8750d2f03ac5759bc15
VishalDhurwey <vldhurwey@gmail.com>
2025-02-17 03:43:53 +0530

    chatbotapi

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -95,7 +95,7 @@ export function ChatBot() {
     setIsLoading(true)
 
     try {
-      const res = await fetch("http://127.0.0.1:5001/api/chat", {
+      const res = await fetch("http://127.0.0.1:5000/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input })
